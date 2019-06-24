6ab034389b6b3e143b4da3a4ae20a9e3e7b7e71f
bunhojun <ponnponn.p@gmail.com>
2019-06-24 17:26:15 +0900

    前進左折右折

--- controller.ts
+++ controller.ts
@@ -1,16 +1,21 @@
 // Add your code here
-//% color="#00ffff"
+
+input.acceleration(Dimension.X)
+//% color="#00bfff"
 namespace コントローラー {
     //% blockId=forward block="前進 %v"
-    export function forward(speed: number): void {
-
-    }    
+    export function forward(shipName: string, speed: number): void {
+        radio.sendValue(shipName + "f", speed);
+        basic.pause(200);
+    }
     //% blockId=right block="右折 %v"
-    export function right(speed: number): void {
-
-    }    
+    export function right(shipName: string, speed: number): void {
+        radio.sendValue(shipName + "r", speed);
+        basic.pause(200);
+    }
     //% blockId=left block="左折 %v"
-    export function left(speed: number): void {
-
+    export function left(shipName: string, speed: number): void {
+        radio.sendValue(shipName + "l", speed);
+        basic.pause(200);
     }
 }

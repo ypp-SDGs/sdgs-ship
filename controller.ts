2fea87678a66fed3c20dd4d60c001042d2d0bacc
bunhojun <ponnponn.p@gmail.com>
2019-06-24 18:28:53 +0900

    ブロック名変更

--- controller.ts
+++ controller.ts
@@ -11,21 +11,21 @@ namespace コントローラー {
         }
     });
 
-    //% blockId=forward block="前進 %v"
+    //% blockId=forward block="$shipName を $speed のスピードで前進させる"
     export function forward(shipName: string, speed: number): void {
         if (isAbleToSend) {
             radio.sendValue(shipName + "f", speed);
             isAbleToSend = false;
         }
     }
-    //% blockId=right block="右折 %v"
+    //% blockId=right block="$shipName を $speed のスピードで右折させる"
     export function right(shipName: string, speed: number): void {
         if (isAbleToSend) {
             radio.sendValue(shipName + "r", speed);
             isAbleToSend = false;
         }
     }
-    //% blockId=left block="左折 %v"
+    //% blockId=left block="$shipName を $speed のスピードで左折させる"
     export function left(shipName: string, speed: number): void {
         if (isAbleToSend) {
             radio.sendValue(shipName + "l", speed);

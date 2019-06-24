b3030df55ce112081c5d79357a836005503dd46e
bunhojun <ponnponn.p@gmail.com>
2019-06-24 18:12:21 +0900

    optimize

--- controller.ts
+++ controller.ts
@@ -3,19 +3,33 @@
 input.acceleration(Dimension.X)
 //% color="#00bfff"
 namespace コントローラー {
+    let isAbleToSend = false;
+    basic.forever(function () {
+        if(!isAbleToSend) {
+            basic.pause(200);
+            isAbleToSend = true;
+        }
+    });
+
     //% blockId=forward block="前進 %v"
     export function forward(shipName: string, speed: number): void {
-        radio.sendValue(shipName + "f", speed);
-        basic.pause(200);
+        if (isAbleToSend) {
+            radio.sendValue(shipName + "f", speed);
+            isAbleToSend = false;
+        }
     }
     //% blockId=right block="右折 %v"
     export function right(shipName: string, speed: number): void {
-        radio.sendValue(shipName + "r", speed);
-        basic.pause(200);
+        if (isAbleToSend) {
+            radio.sendValue(shipName + "r", speed);
+            isAbleToSend = false;
+        }
     }
     //% blockId=left block="左折 %v"
     export function left(shipName: string, speed: number): void {
-        radio.sendValue(shipName + "l", speed);
-        basic.pause(200);
+        if (isAbleToSend) {
+            radio.sendValue(shipName + "l", speed);
+            isAbleToSend = false;
+        }
     }
 }

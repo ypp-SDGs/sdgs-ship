5f3420a283e46bdd339e30183785a64d2d790ccb
bunhojun <ponnponn.p@gmail.com>
2019-06-25 15:27:21 +0900

    deleted unnecessary blocks

--- main.ts
+++ main.ts
@@ -1,11 +1,1 @@
-namespace コメント {
-    //% blockId=show_strings block="コメント %v"
-    export function noaction(text: string): void {
-        
-    }
-
-    //% blockId=somesome block="some %v"
-    export function some(text: string): void {
-        basic.showString(text);
-    }
-}
+ 
